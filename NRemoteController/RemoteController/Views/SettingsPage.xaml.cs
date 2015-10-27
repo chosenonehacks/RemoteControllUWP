c1f1ca39d7007b0d9fe427c3d917cd4551befe33
hal9k2 <hal9k2@hotmail.com>
2015-10-27 13:58:19 +0100

    Initial comit

---
+++ NRemoteController/RemoteController/Views/SettingsPage.xaml.cs
@@ -0,0 +1,18 @@
+using RemoteController.ViewModels;
+using Windows.UI.Xaml.Controls;
+
+namespace RemoteController.Views
+{
+    public sealed partial class SettingsPage : Page
+    {
+        public SettingsPage()
+        {
+            this.InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Disabled;
+        }
+
+        // strongly-typed view models enable x:bind
+        public SettingsPageViewModel ViewModel => this.DataContext as SettingsPageViewModel;
+    }
+}
+
