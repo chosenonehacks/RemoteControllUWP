73db0eaa052db5afe58cadae642b904454dcf32e
hal9k2 <hal9k2@hotmail.com>
2015-10-31 17:49:42 +0100

    Small comments

--- NRemoteController/RemoteController/ViewModels/SettingsPageViewModel.cs
+++ NRemoteController/RemoteController/ViewModels/SettingsPageViewModel.cs
@@ -93,6 +93,9 @@ namespace RemoteController.ViewModels
 
         public List<IPAddress> GetListOfLocalIpAddresses(string iPAddress)
         {
+            //TODO: dodaæ mozliwosc skoanowania zakresów dla innych podsieci
+            //iPAddress = "192.168.2.10";
+
             List<IPAddress> LocalNetworkIpList = new List<IPAddress>();
 
             string stripedIp = iPAddress.Substring(0, iPAddress.LastIndexOf(".", StringComparison.Ordinal) + 1);
