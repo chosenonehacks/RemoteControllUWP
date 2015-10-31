e3ca4ef2cebb1f66727daef0aff231a5ccff2387
hal9k2 <hal9k2@hotmail.com>
2015-10-31 12:46:38 +0100

    Go To Settings Page if no Ip address is setup and added dialog service.

---
+++ NRemoteController/RemoteController/Services/DialogServices/IDialogService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Windows.UI.Popups;
+
+namespace RemoteController.Services.DialogServices
+{
+    public interface IDialogService
+    {
+        Task ShowAsync(string content, string title = default(string));
+        Task ShowAsync(string content, string title, params UICommand[] commands);
+    }
+}
