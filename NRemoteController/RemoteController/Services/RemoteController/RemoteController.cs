c1a5f95563c68a348d71d33827c331b97f6c3958
xorch <hal9k2@outlook.com>
2015-11-17 23:09:13 +0100

    Not working GetChannelListAsync

--- NRemoteController/RemoteController/Services/RemoteController/RemoteController.cs
+++ NRemoteController/RemoteController/Services/RemoteController/RemoteController.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Popups;
 using Windows.Web.Http;
+using Newtonsoft.Json;
+using RemoteController.Models;
 
 namespace RemoteController.Services.RemoteController
 {
@@ -22,7 +24,7 @@ namespace RemoteController.Services.RemoteController
             bool succeeded = false;
             string address = String.Empty;
 
-            if (!string.IsNullOrEmpty(pressedKey) && !string.IsNullOrEmpty(_ipAddress))
+            if (!string.IsNullOrWhiteSpace(pressedKey) && !string.IsNullOrWhiteSpace(_ipAddress))
             {
                 address = "http://" + _ipAddress + "/RemoteControl/KeyHandling/sendKey?key=" + pressedKey;
 
@@ -48,6 +50,46 @@ namespace RemoteController.Services.RemoteController
                 }
             }
             return succeeded = false;
+        }
+
+        public async Task<List<TvChannels>> GetChannelListAsync()
+        {
+            List<TvChannels> tvChannelList = new List<TvChannels>();
+            string address = String.Empty;
+
+            if (!string.IsNullOrWhiteSpace(_ipAddress))
+            {
+                address = "http://" + _ipAddress + "/Live/Channels/getList";
+                
+                var uri = new Uri(address, UriKind.Absolute);
+                
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    try
+                    {
+
+                        //var headers = httpClient.DefaultRequestHeaders;
+
+
+                        //headers.UserAgent.ParseAdd(
+                        //    "Netia%C2%A0Player%C2%A0Pilot/2014.10.271657 CFNetwork/758.1.6 Darwin/15.0.0");
+
+                        
+                        var content = await httpClient.GetStringAsync(uri);
+                        
+
+                        tvChannelList = JsonConvert.DeserializeObject<List<TvChannels>>(content);
+
+                        return tvChannelList;
+
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                }
+            }
+            return tvChannelList;
         }
     }
 }
--- NRemoteController/RemoteController/ViewModels/ListPageViewModel.cs
+++ NRemoteController/RemoteController/ViewModels/ListPageViewModel.cs
@@ -4,22 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Xaml.Navigation;
+using RemoteController.Models;
+using RemoteController.Services.SettingsServiceMyImplementation;
 
 namespace RemoteController.ViewModels
 {
     public class ListPageViewModel : RemoteController.Mvvm.ViewModelBase
     {
+        private readonly ISettingsManager _manager;
+        private readonly Services.RemoteController.RemoteController _remoteController;
+
         public ListPageViewModel()
         {
             if (!Windows.ApplicationModel.DesignMode.DesignModeEnabled)
             {
-
+                _manager = new LocalSettingsManager();
+                _remoteController = new Services.RemoteController.RemoteController(IpAddress);
             }
         }
 
         public override async void OnNavigatedTo(object parameter, NavigationMode mode, IDictionary<string, object> state)
         {
-
+            //await GetChannelsListAsync();
         }
+
+        private List<TvChannels> _listOfTvChannels;
+        public List<TvChannels> ListOfTvChannels
+        {
+            get { return _listOfTvChannels; }
+            set { Set(ref _listOfTvChannels, value); }
+        }
+
+        public async Task GetChannelsListAsync()
+        {
+            ListOfTvChannels = new List<TvChannels>();
+            ListOfTvChannels = _remoteController.GetChannelListAsync().Result;
+        }
+
+        public string IpAddress => _manager.Load<string>("IpSetting", String.Empty);
     }
 }
